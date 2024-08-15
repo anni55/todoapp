c32c5095e93cd7c6b938fdb900d901ed401889df
anni55 <aniketbk.a55@gmail.com>
2024-08-15 20:04:21 +0530

    Added missing images for action buttons

--- src/component/Task.tsx
+++ src/component/Task.tsx
@@ -26,9 +26,9 @@ function Task({taskName, taskDesc, done}:privateProps)
             <div className="flex justify-between ">
                 <textarea onKeyUp={(e)=>{setNewtaskName(e.target.value)}} className={`w-4/5 rounded-xl px-2 p-2 h-11 resize-none outline-none text-black bg-transparent ${!allowEdit && 'bg-slate-200'}`} readOnly = {allowEdit}>{taskName}</textarea>
                 <div>
-                    {/* <button onClick={()=>{taskCompleted(taskName)}} className="w-8 aspect-square object-cover m-2 -translate-y-3 border-2 border-black p-1 rounded">{done && <Image className="" src={require('../img/done.png')} alt="some random text"/>}</button>
+                    <button onClick={()=>{taskCompleted(taskName)}} className="w-8 aspect-square object-cover m-2 -translate-y-3 border-2 border-black p-1 rounded">{done && <Image className="" src={require('../img/done.png')} alt="some random text"/>}</button>
                     <button onClick={()=>{{allowEdit ? togalEdit(setAllowEdit, allowEdit) : edit(setAllowEdit, allowEdit, newtaskName, newTaskDesc)}}} ><Image className="w-7 h-full object-cover m-2" src={require(`../img/${allowEdit ? 'pngwing.com (1).png': 'save.png'}`)} alt="some random text"/></button>
-                    <button onClick={()=>{delet(taskName)}}><Image className="w-7 h-full object-cover m-2" src={require('../img/delet.png')} alt="some random text"/></button> */}
+                    <button onClick={()=>{delet(taskName)}}><Image className="w-7 h-full object-cover m-2" src={require('../img/delet.png')} alt="some random text"/></button>
                 </div>
             </div>
             <textarea onKeyUp={(e)=>{setNewTaskDesc(e.target.value)}} className={`h-28 w-full p-2 rounded-md text-black resize-none bg-transparent outline-none ${!allowEdit && 'bg-slate-200'}`} readOnly = {allowEdit}>{taskDesc}</textarea>
